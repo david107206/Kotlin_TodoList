55dc33729d1b7cf32626ab498721a2289520a174
david107206 <chikdj@naver.com>
2019-08-16 16:48:27 +0900

    - 첫번째 commit

---
+++ TodoList/app/src/main/java/com/dki/toodolist/Todo.kt
@@ -0,0 +1,10 @@
+package com.dki.toodolist
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Todo (
+    @PrimaryKey var id: Long = 0,
+    var title : String = "" ,
+    var date : Long = 0
+)  : RealmObject()
